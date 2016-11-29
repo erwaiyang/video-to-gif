d093e753526fcea8a03e9b31d22c91266b05bfe1
Jean Yang <bdlas321@gmail.com>
2016-11-29 19:27:40 +0800

    setup webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.warn('it works!');
+document.getElementsByTagName('h1')[0].innerHTML = 'Hello Electron';
---
+++ webpack.config.hot.js
@@ -0,0 +1,51 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+const webpack = require('webpack');
+const path = require('path');
+
+const port = process.env.PORT || 8000;
+
+const config = {
+  debug: true,
+  devtool: 'inline-source-map',
+  entry: [
+    // `webpack-hot-middleware/client?path=http://localhost:${port}/__webpack_hmr`,
+    'babel-polyfill',
+    './src/index.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    compress: true,
+    stats: {
+      colors: true,
+      chunks: false,
+    },
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: ['node_modules'],
+      },
+    ],
+  },
+  resolve: {
+    root: path.resolve(__dirname),
+    extensions: ['', '.js', '.jsx', '.css'],
+    modulesDirectories: ['node_modules'],
+    alias: {},
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
+};
+
+module.exports = config;
